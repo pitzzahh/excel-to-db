1e8f05895c15f6b6b159c5067e39ba4b98e90c9d
Peter John Arao <pitzzahh@gmail.com>
2024-03-23 00:02:01 +0800

    :art: Improve code

--- src/main/java/me/araopj/Main.java
+++ src/main/java/me/araopj/Main.java
@@ -3,6 +3,7 @@ package me.araopj;
 import com.google.gson.GsonBuilder;
 import io.github.pitzzahh.util.utilities.FileUtil;
 import io.github.pitzzahh.util.utilities.Print;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -10,46 +11,67 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static io.github.pitzzahh.util.utilities.Print.println;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        var file = new File("MOCK_DATA.csv");
-        var data = getFileContents(file, 1, ';', StandardCharsets.UTF_8);
+    public static void main(String[] args) {
+        final var trance = new HashMap<Integer, Integer>();
 
-        List<List<Salary>> salaries = data.stream()
-                .map(arr -> IntStream.range(1, arr.length).mapToObj(i -> Salary.builder()
-                        .salaryGrade(Integer.parseInt(arr[0]))
-                        .step(i)
-                        .amount(Objects.equals("BLANK", arr[i]) ? 0 : convertToDouble(arr[i]))
-                        .year(2021)
-                        .tranche(2)
-                        .build()).collect(Collectors.toList())
-                ).toList();
+        trance.put(2021, 2);
+        trance.put(2022, 3);
+        trance.put(2023, 4);
 
-        println("Salaries for the year 2021");
+        Arrays.stream(new String[]{"2021.csv", "2022.csv", "2023.csv"})
+                .map(File::new) // Convert the string to a File (the string is the file name with extension)
+                .forEach(file -> {
+                    var fileName = file.getName().replace(".csv", "");
+                    if (!file.exists()) {
+                        throw new IllegalArgumentException("File " + fileName + " does not exist");
+                    }
+                    try {
+                        var rawData = getFileContents(file, 1, ';', StandardCharsets.UTF_8);
 
-        salaries.forEach(s -> {
-                    println("-".repeat(100));
-                    println("Salary steps = " + s.size());
-                    println("First salary = " + s.getFirst());
-                    println("Last salary = " + s.getLast());
-                    s.forEach(Print::println);
-                    println("-".repeat(100));
+                        // Convert the raw data to a list of salaries
+                        // A Salary can have multiple steps
+                        // We store the salaries based on their salary grade
+                        // Each salary grade has multiple steps, so we store them in a list
+                        List<List<Salary>> salaries = rawData.stream()
+                                .map(arr -> IntStream.range(1, arr.length) // Skip the first element which is the salary grade
+                                        .mapToObj(i -> Salary.builder()
+                                                .salaryGrade(Integer.parseInt(arr[0]))
+                                                .step(i)
+                                                .amount(Objects.equals("BLANK", arr[i]) ? 0 : convertToDouble(arr[i]))
+                                                .year(Integer.parseInt(fileName))
+                                                .tranche(trance.getOrDefault(Integer.parseInt(fileName), -1))
+                                                .build()
+                                        ).collect(Collectors.toList())
+                                ).toList();
+
+                        println("Salaries for the year " + fileName);
+
+                        salaries.forEach(s -> {
+                            println("-".repeat(100));
+                            println("Salary steps = " + s.size());
+                            println("First salary = " + s.getFirst());
+                            println("Last salary = " + s.getLast());
+                            s.forEach(Print::println);
+                            println("-".repeat(100));
+                        });
+
+                        println("-------CONVERTING TO JSON---------");
+                        var gson = new GsonBuilder().setPrettyPrinting()
+                                .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+                                .create();
+                        var json = gson.toJson(salaries);
+                        FileUtil.writeToATextFile(json, new File(String.format("%s.json", fileName)), false);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 });
-
-        println("-------CONVERTING TO JSON---------");
-        var gson = new GsonBuilder().setPrettyPrinting()
-                .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
-                .create();
-        var json = gson.toJson(salaries);
-        FileUtil.writeToATextFile(json, new File("mock_data.json"), false);
     }
 
     /**
