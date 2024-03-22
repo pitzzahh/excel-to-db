8b3453f1d7f356ca998952c22bd67432e53570ca
Peter John Arao <pitzzahh@gmail.com>
2024-03-22 23:11:00 +0800

    :sparkles: Add Salary class

--- src/main/java/me/araopj/Salary.java
+++ src/main/java/me/araopj/Salary.java
@@ -2,12 +2,13 @@ package me.araopj;
 
 import lombok.Builder;
 import lombok.Data;
-import java.time.LocalDate;
 
 @Data
 @Builder
 public class Salary {
     private int salaryGrade;
-    private String stepName;
-    private double stepAmount;
+    private int step;
+    private double amount;
+    private int year;
+    private int tranche;
 }
